cfe5980075035b4da8c676ecdf949149efdf6219
Zoltan Horvath <zoltan.horvath@proxybit.hu>
2022-01-21 18:15:23 +0100

    add basic navbar items

--- resources/views/main-page.blade.php
+++ resources/views/main-page.blade.php
@@ -18,7 +18,11 @@
         <a class="navbar-brand" href="#">File Manager App</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Users</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Documents</a></li>
+                    <li class="nav-item "><a class="nav-link" href="#">Log Out</a></li>
+            </ul>
         </div>
     </div>
 </nav>
